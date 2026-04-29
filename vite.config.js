76a5c95dc3091c2e315e0dd00d28ed6c90dc58a7
unknown <febyyohaana@gmail.com>
2026-04-29 15:49:04 +0700

    Fix GitHub Pages base path from /dms/ to /dmsk/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dms/',
+  base: '/dmsk/',
   plugins: [react()],
   build: {
     outDir: 'docs',
